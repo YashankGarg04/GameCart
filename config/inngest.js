f7850ae1d46ae4edb14fc25331bd217706cff8e1
Yashank <yashankgarg04@gmail.com>
2025-03-24 12:02:24 +0530

    final order

--- config/inngest.js
+++ config/inngest.js
@@ -69,13 +69,13 @@ export const createUserOrder = inngest.createFunction(
     },
     {event: 'order/created'},
     async ({events}) => {
-        const orders = events.map(()=> {
+        const orders = events.map((event)=> {
             return {
-                userId: Event.data.userId,
-                items: Event.data.items,
-                amount: Event.data.amount,
-                address: Event.data.address,
-                date: Event.data.date
+                userId: event.data.userId,
+                items: event.data.items,
+                amount: event.data.amount,
+                address: event.data.address,
+                date: event.data.date
             }
         })
 
